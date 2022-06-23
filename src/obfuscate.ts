49cc3cb0b6f8f8cd06012aca2c14587a2e3d7aaf
bernhard <schoenbergerb@mail.de>
2022-06-23 22:43:54 +0200

    add default options

--- src/obfuscate.ts
+++ src/obfuscate.ts
@@ -1,5 +1,5 @@
-import { load } from "opentype.js";
-import { ObfuscationOptions } from "./obfuscation-options";
+import { loadSync } from "opentype.js";
+import { DEFAULT_OPTIONS, ObfuscationOptions } from "./obfuscation-options";
 import { obfuscateGlyphs } from "./glyph";
 import { generateObfuscatedFont } from "./font";
 import value2glyphs from "./value2glyphs";
@@ -17,16 +17,17 @@ export async function obfuscate<T extends string | number | object>(
   fontFilePath: string,
   options?: ObfuscationOptions
 ): Promise<{ value: T, font: Buffer }> {
-  const font = await load(fontFilePath, null, {});
+  const { characterRange, strength, lowMemory } = options ?? DEFAULT_OPTIONS;
+
+  const font = loadSync(fontFilePath, { lowMemory: lowMemory ?? false });
 
   const originalGlyphs = value2glyphs(value, font);
 
-  const { translation, glyphs } = obfuscateGlyphs(originalGlyphs, options);
+  const { translation, glyphs } = obfuscateGlyphs(originalGlyphs, characterRange, strength);
 
   const buffer = generateObfuscatedFont(font, glyphs);
 
   const obj = obfuscateValue(value, translation);
-
   return {
     value: obj as T,
     font: buffer,
--- src/obfuscation-options.ts
+++ src/obfuscation-options.ts
@@ -2,14 +2,26 @@ import { EncryptionCharacterRange } from "./encryption-character-range.enum";
 
 export interface ObfuscationOptions {
   /**
-   * obfuscation strength multiplier (default: 1)
+   * optional: obfuscation strength multiplier (default: 1)
    * all under 0.1 makes no sense (can simply be rounded and so back calculated)
    * all over 10 makes no sense (looks like 💩)
    */
   strength?: number;
 
   /**
-   *  the character-range to pick obfuscation glyphs from
+   * optional: the character-range to pick obfuscation glyphs from
    */
   characterRange?: EncryptionCharacterRange;
+
+  /**
+   * optional: lowMemory option
+   * use if to less memory availible for loading font
+   */
+   lowMemory?: boolean;
+}
+
+export const DEFAULT_OPTIONS: ObfuscationOptions = {
+  strength: 1,
+  characterRange: EncryptionCharacterRange.PRIVATE_USE_AREA,
+  lowMemory: false,
 }
