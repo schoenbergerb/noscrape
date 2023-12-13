8514a4fcd5769d97fc9b17fb3cdc6954bb6da978
Bernhard <mail@schoenberger.be>
2023-12-13 22:26:38 +0100

    update demo

--- example/demo.ts
+++ example/demo.ts
@@ -1,32 +1,36 @@
 import { obfuscate } from '../src';
-import { EncryptionCharacterRange } from '../src/encryption-character-range.enum';
-import fs from 'fs';
+import { EncryptionCharacterRange } from '../src';
 import express from 'express';
+import {Noscrape} from "../src/noscrape";
 
 const app = express()
 
 app.get('/', (req, res) => {
-    obfuscate(
-        { data: "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"§$%&/()=¹²³¼½¬{[]},.-;:_·…–<>|"}, 
-        __dirname + "/example.ttf",
-        { characterRange: EncryptionCharacterRange.PRIVATE_USE_AREA }
-    ).then(({value, font}) => {
-        res.send(`
-        <html>
+    const noscrape = new Noscrape(__dirname + "/example.ttf");
+
+    const test3 = noscrape.obfuscate({ data: "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"§$%&/()=¹²³¼½¬{[]},.-;:_·…–<>|"});
+    const test1= noscrape.obfuscate("test1");
+    const test2= noscrape.obfuscate(123456789);
+
+    // language=HTML
+    res.send(`
+        <html lang="en">
             <head>
+                <title>Noscrape - DEMO</title>   
                 <style>
                     @font-face {        
                         font-family: 'noscrape-obfuscated';        
-                        src: url('data:font/truetype;charset=utf-8;base64,${font.toString('base64')}');    
+                        src: url('data:font/truetype;charset=utf-8;base64,${noscrape.getFont().toString('base64')}');    
                     }
-                </style>      
+                </style>   
             </head>
             <body style="font-family: 'noscrape-obfuscated'">
-                ${value.data}
+                <p>${test1}</p>
+                <p>${test2}</p>
+                <p>${test3.data}</p>
             </body>
         </html>    
     `)
-    })
 })
 
 
