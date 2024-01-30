e7ebca88b7b4af5d2d863ec7d5897b82c3cacce1
Bernhard <mail@schoenberger.be>
2024-01-30 23:22:14 +0100

    update demo for visual compare

--- example/demo.ts
+++ example/demo.ts
@@ -1,35 +1,72 @@
 import express from 'express';
 import {Noscrape} from "../src";
+import * as fs from "fs";
 
 const app = express()
+
+app.get('/example.ttf', (_, res) => {
+    res.sendFile(__dirname + '/example.ttf')
+})
 
 app.get('/', (req, res) => {
     const noscrape = new Noscrape(__dirname + "/example.ttf");
 
-    const test1= noscrape.obfuscate("test1");
-    const test2= noscrape.obfuscate(123456789);
-    const test3 = noscrape.obfuscate({ data: "a-nice-object" });
-    const test4 = noscrape.obfuscate( "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"§$%&/()=¹²³¼½¬{[]},.-;:_·…–<>|");
+
+    const test1 = noscrape.obfuscate("test1");
+    const test2 = noscrape.obfuscate(123456789);
+    const test3 = noscrape.obfuscate({data: "a-nice-object"});
+    const test4 = noscrape.obfuscate("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"§$%&/()=¹²³¼½¬{[]},.-;:_·…–<>|");
 
     // language=HTML
     res.send(`
         <html lang="en">
-            <head>
-                <title>Noscrape - DEMO</title>   
-                <style>
-                    @font-face {        
-                        font-family: 'noscrape-obfuscated';        
-                        src: url('data:font/truetype;charset=utf-8;base64,${noscrape.getFont().toString('base64')}');    
-                    }
-                </style>   
-            </head>
-            <body style="font-family: 'noscrape-obfuscated'">
-                <p>${test1}</p>
-                <p>${test2}</p>
-                <p>${test3.data}</p>
-                <p>${test4}</p>
-            </body>
-        </html>    
+        <head>
+            <title>Noscrape - DEMO</title>
+            <style>
+                @font-face {
+                    font-family: 'original-font';
+                    src: url('/example.ttf');
+                }
+
+                @font-face {
+                    font-family: 'noscrape-obfuscated';
+                    src: url('data:font/truetype;charset=utf-8;base64,${noscrape.getFont().toString('base64')}');
+                }
+            </style>
+        </head>
+        <body>
+        <table id="original">
+            <tr>
+                <td style="font-family: 'original-font'">test1</td>
+            </tr>
+            <tr>
+                <td style="font-family: 'original-font'">123456789</td>
+            </tr>
+            <tr>
+                <td style="font-family: 'original-font'">a-nice-object</td>
+            </tr>
+            <tr>
+                <td style="font-family: 'original-font'">
+                    abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"§$%&/()=¹²³¼½¬{[]},.-;:_·…–<>|
+                </td>
+            </tr>
+        </table>
+        <table id="obfuscated">
+            <tr>
+                <td style="font-family: 'noscrape-obfuscated'">${test1}</td>
+            </tr>
+            <tr>
+                <td style="font-family: 'noscrape-obfuscated'">${test2}</td>
+            </tr>
+            <tr>
+                <td style="font-family: 'noscrape-obfuscated'">${test3.data}</td>
+            </tr>
+            <tr>
+                <td style="font-family: 'noscrape-obfuscated'">${test4}</td>
+            </tr>
+        </table>
+        </body>
+        </html>
     `)
 })
 
